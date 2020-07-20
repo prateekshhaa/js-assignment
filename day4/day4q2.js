b43214b5282bcdd3074e1cae936a0948183dfeda
prateekshhaa <68272000+prateekshhaa@users.noreply.github.com>
2020-07-20 15:58:44 +0530

    assignment of day 4

---
+++ day4/day4q2.js
@@ -0,0 +1,12 @@
+const student={
+    name: "Helsinki",
+    age: 24,
+    projects:{
+        diceGame:"Two player dice game using Javascript"
+    }
+}
+const {name, age, projects} = student;
+
+console.log(name); 
+console.log(age);
+console.log(projects); 
---
+++ day4/day4q3.js
@@ -0,0 +1,12 @@
+
+var shoppingList = ["Milk","Eggs","Bread","Onions","Potatoes"];
+
+for(var i=0;i<shoppingList.length;i++)
+{
+    var item = shoppingList[i];
+}
+var shoppingBasket = ["Milk","Eggs","Bread","Onions","Potatoes"];
+shoppingBasket.push("Cookies");
+shoppingBasket.push("Meat");
+shoppingBasket.push("Brownie");
+console.log(shoppingBasket);
